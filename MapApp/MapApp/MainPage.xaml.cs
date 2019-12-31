d5d6f874d4475bcec5478d9d071d15e10d71b8cb
shuheydev <shuhey.dev@gmail.com>
2019-12-31 23:46:34 +0900

    Add project files.

---
+++ MapApp/MapApp/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Newtonsoft.Json;
+using System.IO;
+using System.Reflection;
+using Xamarin.Forms.Maps;
+using System.Diagnostics;
+
+namespace MapApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //Mapの描画が終わるのを待っている?
+            Task.Delay(2000);
+            UpdateMap();
+        }
+
+
+        List<Place> placesList = new List<Place>();
+
+        private void UpdateMap()
+        {
+            try
+            {
+                //Embedded resourceのPlaces.jsonを読み込む.文字列として
+                var assembly = IntrospectionExtensions.GetTypeInfo(typeof(MainPage)).Assembly;
+                Stream stream = assembly.GetManifestResourceStream("MapApp.Places.json");
+                string text = string.Empty;
+                using (var reader = new StreamReader(stream))
+                {
+                    text = reader.ReadToEnd();
+                }
+
+                //文字列のJsonをPlacesオブジェクトに変換する
+                var resultObject = JsonConvert.DeserializeObject<Places>(text);
+
+                //場所の情報PlaceをListに追加する.
+                foreach (var place in resultObject.results)
+                {
+                    placesList.Add(new Place
+                    {
+                        PlaceName = place.name,
+                        Address = place.vicinity,
+                        Location = place.geometry.location,
+                        Position = new Position(place.geometry.location.lat, place.geometry.location.lng),
+                        //Icon = place.icon,
+                        //Distance = $"{GetDistance(lat1, lon1, place.geometry.location.lat, place.geometry.location.lng, DistanceUnit.Kiliometers).ToString("N2")}km",
+                        //OpenNow = GetOpenHours(place?.opening_hours?.open_now)
+                    });
+                }
+
+                MyMap.ItemsSource = placesList;
+                //PlacesListView.ItemsSource = placesList;
+                //var loc = await Xamarin.Essentials.Geolocation.GetLocationAsync();
+                MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(47.6370891183, -122.123736172), Distance.FromKilometers(100)));
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+        }
+    }
+}
